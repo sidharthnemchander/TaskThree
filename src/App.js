aaf9cce9f74e3d0048edd5539181b3ca6fa3947f
sidharthnemchander <sidharthchander62@gmail.com>
2026-01-06 20:58:04 +0530

    Integrating slider , popus

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import Board from "./components/board";
+import Board from "./components/Board";
 import Dash from "./components/Dash";
+import GameOver from "./components/GameOver";
+import HelpSlider from "./components/HelpSlider";
+import MessageBox from "./components/MessageBox";
+import WinMessage from "./components/WinMessage";
 
 function App() {
   const [word, setWord] = useState("");
   const [gameStatus, setGameStatus] = useState("playing");
+  const [showHelp, setShowHelp] = useState(false);
   const [message, setMessage] = useState("");
 
   useEffect(() => {
@@ -28,6 +33,11 @@ function App() {
   const handleWin = () => {
     setGameStatus("win");
   };
+
+  const handleHelpClick = () => {
+    setShowHelp(!showHelp);
+  };
+
   const showMessage = (msg) => {
     setMessage(msg);
     setTimeout(() => setMessage(""), 2000);
@@ -45,6 +55,13 @@ function App() {
         onWin={handleWin}
         showMessage={showMessage}
       />
+      {gameStatus === "lose" && <GameOver word={word} gameover={true} />}
+      <MessageBox message={message} />
+      {gameStatus === "win" && <WinMessage gameover={true} />}
+      <button id="help" onClick={handleHelpClick}>
+        ?
+      </button>
+      {showHelp && <HelpSlider />}
     </div>
   );
 }
